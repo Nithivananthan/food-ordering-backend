225431eed791bb494f98ba366dfb9baa5841d6c9
Nithivananthan <mnithivananthan@gmail.com>
2026-05-16 18:36:49 +0530

    first commit

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Connection error:", err));
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use("/api/categories",CategoriesRoutes);
   app.use('/api/foodinformation',foodinforamation)
 app.use('/api/orders',order)
 app.use('/api/user',user) 
 app.use('/api/admin',admin)
-app.use(cors())
+
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running on port ${port}`))
